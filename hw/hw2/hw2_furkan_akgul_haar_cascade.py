2425a440dcb53360c0d6a62ba9553123c47d9372
mtkoroglu <m.tahakoroglu@gmail.com>
2022-06-11 00:29:42 +0300

    Add files via upload

--- hw/hw2/hw2_furkan_akgul_haar_cascade.py
+++ hw/hw2/hw2_furkan_akgul_haar_cascade.py
@@ -13,15 +13,21 @@ for i in range(47,114): # furkan-data-1 için 66, furkan-data-2 için 47,114,1
     gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
     rects1 = detector.detectMultiScale(gray1, scaleFactor=1.05, minNeighbors=25)
     rects2 = detector.detectMultiScale(gray2, scaleFactor=1.05, minNeighbors=25)
+    # tespit edilen yüzleri dikdörtgen olarak çiz ve piksel koordinatlarını yaz
     for (x,y,width,height) in rects1:
         cv2.rectangle(img1, (x,y), (x+width,y+height), (0,255,0), 2)
+        cv2.putText(img1,'(%.2f,%.2f)' %(x+width/2,y+height/2),(x-30,y-10),2,0.45,(0,255,0),1)
+    cv2.putText(img1, 'Tespit edilen yuz sayisi: %i' %len(rects1), (20,460), 2, 0.6, (0,255,0), 1)
     for (x,y,width,height) in rects2:
         cv2.rectangle(img2, (x,y), (x+width,y+height), (0,255,0), 2)
+        cv2.putText(img2, '(%.2f,%.2f)' %(x+width/2,y+height/2), (x-30,y-10), 2, 0.45, (0,255,0), 1)
+    cv2.putText(img2, 'Tespit edilen yuz sayisi: %i' %len(rects2), (20,460), 2, 0.6, (0,255,0), 1)
+    # resmi göster
     stereo[:,0:w,:] = img1
     stereo[:,w:2*w,:] = img2
     s = 0.8
     rstereo = cv2.resize(stereo, (int(s*stereo.shape[1]), int(s*stereo.shape[0])), cv2.INTER_LINEAR)
-    cv2.imshow('Haar Cascade metodu ile yuz tespiti', rstereo)
+    cv2.imshow('Haar cascade metodu ile yuz tespiti', rstereo) 
     if cv2.waitKey(1) == 27: # ESC'ye basınca çık
         break
 cv2.destroyAllWindows()
